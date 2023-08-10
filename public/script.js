0d48c8dc88f1a92f196bb8851a4ee4e101644087
Bailee <baileebyu@gmail.com>
2023-08-10 09:47:06 -0600

    Changed light/dark theme control to toggle switch

--- public/script.js
+++ public/script.js
@@ -1,10 +1,7 @@
 // setting theme
 const setTheme = theme => document.documentElement.className = theme;
 
-document.getElementById('theme-select').addEventListener('change', function() {
-  setTheme(this.value);
-		// if (this.value == "secret"){
-		// 	console.log("secret");
-		// 	window.location='secret.html';
-		// }
+document.getElementById('theme-toggle').addEventListener('change', function() {
+  setTheme(this.checked ? 'dark' : 'yellow');
 });
+
