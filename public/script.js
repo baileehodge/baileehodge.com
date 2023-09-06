14c8e0142e0cf06f74be042e97b9b17763469224
Bailee <baileebyu@gmail.com>
2023-09-06 15:23:17 -0600

    added resume link

--- public/script.js
+++ public/script.js
@@ -2,6 +2,13 @@
 const setTheme = theme => document.documentElement.className = theme;
 
 document.getElementById('theme-toggle').addEventListener('change', function() {
-  setTheme(this.checked ? 'dark' : 'yellow');
+	setTheme(this.checked ? 'dark' : 'yellow');
+});
+
+const menuItems = document.querySelectorAll('#menu-items a');
+menuItems.forEach(item => {
+	item.addEventListener('click', () => {
+    	document.getElementById('menu-toggle').checked = false;
+	});
 });
 
