ee5aae7aa2dc8e952d929c9c8ef34ac35a7ed3e8
Bailee <baileebyu@gmail.com>
2024-10-09 10:48:13 -0600

    updated demo planner, hosted it on /planner

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 // setting theme
 const setTheme = theme => document.documentElement.className = theme;
 
-document.getElementById('theme-toggle').addEventListener('change', function() {
+document.getElementById('theme-toggle').addEventListener('change', function () {
 	setTheme(this.checked ? 'dark' : 'yellow');
 });
 
 const menuItems = document.querySelectorAll('#menu-items a');
 menuItems.forEach(item => {
 	item.addEventListener('click', () => {
-    	document.getElementById('menu-toggle').checked = false;
+		document.getElementById('menu-toggle').checked = false;
 	});
 });
 
