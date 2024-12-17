aea45497fc3ce7ccafef352deedfe21a73a6d1da
Bailee <baileebyu@gmail.com>
2024-12-17 11:04:35 -0700

    Huge progress on the cards redesign. Next task is to design and make the clickable cards

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 // setting theme
 const setTheme = theme => document.documentElement.className = theme;
 
+
 document.getElementById('theme-toggle').addEventListener('change', function () {
-	setTheme(this.checked ? 'dark' : 'yellow');
+	setTheme(this.checked ? 'yellow' : 'dark');
 });
 
 const menuItems = document.querySelectorAll('#menu-items a');
